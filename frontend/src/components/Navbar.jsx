3debffdb1b6a7e121674a546d07d3157afc0262d
lin001234 <162286563+lin001234@users.noreply.github.com>
2025-06-11 22:00:52 +0800

    Added auth logic for navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,41 +1,75 @@
 // For reusable components
+import { useState, useEffect } from 'react';
 import { Navbar, Nav, Container, Button, Modal} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import LogOut from '../features/auth/pages/logout';
+import { axiosInstance } from './axios';
 
 function NavigationBar() {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
-  return (
-    <>
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() =>{
+    const fetchAuth= async()=>{
+      try{
+        const response=await axiosInstance.get('/auth/status');
+        setAuthenticated(response.data.authenticated);
+      }catch(err){
+        console.error('Failed to get Authentication', err);
+      }
+    };
+    fetchAuth();
+  },[]);
+
+  if(authenticated){
+    return (
+      <> 
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/">NTU Stars Swapper</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/post/filter">Search</Nav.Link>
+                <Nav.Link as={Link} to="/post/self">Own Posts</Nav.Link>
+                <Nav.Link as={Link} to="/create">Create Post</Nav.Link>
+                <Button 
+                    variant="outline-light" 
+                    className="ms-2"
+                    onClick={() => setShowLogoutModal(true)}
+                  >
+                    Logout
+                </Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <LogOut 
+            show={showLogoutModal} 
+            onHide={() => setShowLogoutModal(false)} 
+          />
+      </>
+    );
+  }
+  else{
+    return(
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand as={Link} to="/">NTU Stars Swapper</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/post/filter">Search</Nav.Link>
-              <Nav.Link as={Link} to="/post/self">Own Posts</Nav.Link>
-              <Nav.Link as={Link} to="/create">Create Post</Nav.Link>
-              <Button 
-                  variant="outline-light" 
-                  className="ms-2"
-                  onClick={() => setShowLogoutModal(true)}
-                >
-                  Logout
-              </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+          <Container>
+            <Navbar.Brand as={Link} to="/">NTU Stars Swapper</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/">Login</Nav.Link>
+                <Nav.Link as={Link} to="/post/filter">Sign Up</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
       </Navbar>
 
-      <LogOut 
-          show={showLogoutModal} 
-          onHide={() => setShowLogoutModal(false)} 
-        />
-    </>
-  );
+    )
+  }
 }
 
 export default NavigationBar; 
